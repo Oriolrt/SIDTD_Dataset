e53b4dbe86c2709023ca619b264e19a6924724d6
Bycarkos <cbonedriera@gmail.com>
2022-10-06 13:35:47 +0200

    desacoplated

--- Fake_Generator/Fake_Loader/utils.py
+++ Fake_Generator/Fake_Loader/utils.py
@@ -13,42 +13,35 @@ import imageio
 import matplotlib.pyplot as plt
 
 
-def replace_info_documents(im0, im1, data0, data1,dx1,dy1,dx2,dy2):
-    # take the coordinates of the region to replace
-    coord0 = np.array(data0['quad'], dtype=np.float32())
-    coord1 = np.array(data1['quad'], dtype=np.float32())
+def get_optimal_font_scale(text, width):
+    fontsize = 1  # starting font size
+    sel_font =  get_font_scale()#"/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+    stop  = False  # portion of image width you want text width to be
+    img_fraction = 1
+    try:
+        font = ImageFont.truetype(font=sel_font, size=fontsize,encoding="unic")
+    except:
+        sel_font = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+        font = ImageFont.truetype(font=sel_font, size=fontsize,encoding="unic")
 
-    # Homography: p1 = H@p0 | p0 = inv(H)@p1
-    # (where @ denotes matricial product, inv the inverse of the homography
-    # and p0 and p1 are homogeneous coordinates for the pixel coordinates)
+    while (font.getsize(text)[0] < img_fraction*width) and (stop == False):
+        # iterate until the text size is just larger than the criteria
+        if font.getsize(text)[0] == 0:
+            sel_font =  "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
+ 
+            if font.getsize(text)[1] == 0:
+                stop = True
+                break
 
-    H, mask = cv2.findHomography(coord0, coord1, cv2.RANSAC, 1.0)
+        fontsize += 1
+        font = ImageFont.truetype(sel_font, fontsize,encoding="unic")
 
-    im1_rep = replace_one_document(im0, im1, coord0, H,dx1,dy1,dx2,dy2)
+    # optionally de-increment to be sure it is less than criteria
+    fontsize -= 1
+    font = ImageFont.truetype(sel_font, fontsize,encoding="unic")
 
-    im0_rep = replace_one_document(im1, im0, coord1, np.linalg.inv(H),dx1,dy1,dx2,dy2)
+    return font
 
-    return im0_rep, im1_rep
-
-def replace_one_document(im_a, im_b, coord_a, H,dx1,dy1,dx2,dy2):
-
-    mask_a = np.zeros_like(im_a)
-    cv2.drawContours(mask_a, [coord_a.astype(int)], -1, color=(255, 255, 255), thickness=cv2.FILLED)
-    y_a, x_a = np.where((mask_a[..., 0] / 255).astype(int) == 1)
-    coordh_a = np.ones((3, len(x_a)), dtype=np.float32())
-    coordh_a[0, :] = x_a
-    coordh_a[1, :] = y_a
-
-    coordh_b = H @ coordh_a
-    coordh_b = coordh_b / coordh_b[-1, ...]
-
-    x_b = coordh_b.T[:, 0].astype(int)
-    y_b = coordh_b.T[:, 1].astype(int)
-
-    im_rep = copy.deepcopy(im_b)
-    im_rep[y_b +  dy1, x_b + dx1, ...] = im_a[y_a + dy2, x_a + dx2, ...]
-
-    return im_rep
 
 def get_font_scale(inner_path: str = "/usr/share/fonts/TTF"):
 
@@ -58,14 +51,14 @@ def get_font_scale(inner_path: str = "/usr/share/fonts/TTF"):
 
     return os.path.join(inner_path,selected)
 
-def mask_from_info(img, shape:np.ndarray,shaped = False):
+def mask_from_info(img, shape:np.ndarray,flag:int=1,shaped:bool = False):
     def midpoint(x1, y1, x2, y2):
         x_mid = int((x1 + x2) / 2)
         y_mid = int((y1 + y2) / 2)
         return (x_mid, y_mid)
 
     if not shaped:
-        x0, y0, w, h = bbox_info(shape)
+        x0, y0, w, h = bbox_info(shape, flag)
         shape = bbox_to_coord(x0, y0, w, h)
 
     x0, x1, x2, x3 = shape[0][0], shape[1][0], shape[2][0], shape[3][0]
@@ -143,8 +136,25 @@ def store(img_loader: list,path_store="/home/carlos/MIDV2020/Fake_Benchmark_Gene
         
     print("Data Successfuly stored")
 
+def bbox_to_coord(x, y, w, h):
+    x_f = x + w
+    y_f = y + h
 
-def midv500_bbox_info(info):
+    c1, c2, c3, c4 = [x, y], [x_f, y], [x_f, y_f], [x, y_f]
+
+    return [c1, c2, c3, c4]
+
+def bbox_info(info, flag):
+    
+    if flag == 1:
+        shape = info["shape_attributes"]
+        x = shape["x"]
+        y = shape["y"]
+        w = shape["width"]
+        h = shape["height"]
+        
+        return x,y,w,h
+          
     shape = info["quad"]
     x0, x1, x2, x3 = shape[0][0], shape[1][0], shape[2][0], shape[3][0]
     y0, y1, y2, y3 = shape[0][1], shape[1][1], shape[2][1], shape[3][1]
@@ -154,17 +164,25 @@ def midv500_bbox_info(info):
 
     return x0, y0, w, h
 
-#midv2020 functions (to clean)
 
-def compute_homography_2020(info1, info2):
+def compute_homography(info1, info2, flag:int=1, mixed:bool=False):
     # take the coordinates of the region to replace
-    x, y, w, h = bbox_info(info1)
-    x2, y2, w2, h2 = bbox_info(info2)
-
+    if not mixed:   
+        x, y, w, h = bbox_info(info1, flag=flag)
+        x2, y2, w2, h2 = bbox_info(info2, flag=flag)
+    else:
+        try:
+            x, y, w, h = bbox_info(info1, flag=flag)
+            x2, y2, w2, h2 = bbox_info(info2, flag=abs(flag-1))
+        except:
+            x, y, w, h = bbox_info(info1, flag=abs(flag-1))
+            x2, y2, w2, h2 = bbox_info(info2, flag=flag)
+    
+    
+                       
     coord0 = np.array(bbox_to_coord(x, y, w, h), dtype=np.float32())
     coord1 = np.array(bbox_to_coord(x2, y2, w2, h2), dtype=np.float32())
-
-
+    
     # Homography: p1 = H@p0 | p0 = inv(H)@p1
     # (where @ denotes matricial product, inv the inverse of the homography
     # and p0 and p1 are homogeneous coordinates for the pixel coordinates)
@@ -173,9 +191,29 @@ def compute_homography_2020(info1, info2):
 
     return H, coord0, coord1, mask
 
-#im_a, im_b, coord_a, H,dx1,dy1,dx2,dy2
-def replace_info_documents_2020(im0, im1, data0, data1, delta1, delta2):
-    H, coord0, coord1, _ = compute_homography_2020(data0, data1)
+
+def replace_one_document(im_a, im_b, coord_a, H,dx1,dy1,dx2,dy2):
+
+    mask_a = np.zeros_like(im_a)
+    cv2.drawContours(mask_a, [coord_a.astype(int)], -1, color=(255, 255, 255), thickness=cv2.FILLED)
+    y_a, x_a = np.where((mask_a[..., 0] / 255).astype(int) == 1)
+    coordh_a = np.ones((3, len(x_a)), dtype=np.float32())
+    coordh_a[0, :] = x_a
+    coordh_a[1, :] = y_a
+
+    coordh_b = H @ coordh_a
+    coordh_b = coordh_b / coordh_b[-1, ...]
+
+    x_b = coordh_b.T[:, 0].astype(int)
+    y_b = coordh_b.T[:, 1].astype(int)
+
+    im_rep = copy.deepcopy(im_b)
+    im_rep[y_b +  dy1, x_b + dx1, ...] = im_a[y_a + dy2, x_a + dx2, ...]
+
+    return im_rep
+
+def replace_info_documents(im0, im1, data0, data1, delta1, delta2, flag:int=1, mixed:bool=False):
+    H, coord0, coord1, _ = compute_homography(data0, data1, flag=flag, mixed=mixed)
 
     dx1,dy1 = delta1
     dx2,dy2 = delta2
@@ -185,65 +223,3 @@ def replace_info_documents_2020(im0, im1, data0, data1, delta1, delta2):
     im0_rep = replace_one_document(im1, im0, coord1, np.linalg.inv(H), dx1,dy1,dx2,dy2)
 
     return im0_rep, im1_rep
-
-def bbox_info(info):
-    shape = info["shape_attributes"]
-
-    x = shape["x"]
-    y = shape["y"]
-    w = shape["width"]
-    h = shape["height"]
-
-    return x, y, w, h
-
-
-def bbox_to_coord(x, y, w, h):
-    x_f = x + w
-    y_f = y + h
-
-    c1, c2, c3, c4 = [x, y], [x_f, y], [x_f, y_f], [x, y_f]
-
-    return [c1, c2, c3, c4]
-
-
-#Function for midv500 to clean
-def compute_homography(src_points, dst_points):
-
-    # Homography: p1 = H@p0 | p0 = inv(H)@p1
-    # (where @ denotes matricial product, inv the inverse of the homography
-    # and p0 and p1 are homogeneous coordinates for the pixel coordinates)
-
-    H, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC, 1.0)
-
-    return H, mask
-
-
-def get_optimal_font_scale(text, width):
-    fontsize = 1  # starting font size
-    sel_font =  get_font_scale()#"/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
-    stop  = False  # portion of image width you want text width to be
-    img_fraction = 1
-    try:
-        font = ImageFont.truetype(font=sel_font, size=fontsize,encoding="unic")
-    except:
-        sel_font = "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
-        font = ImageFont.truetype(font=sel_font, size=fontsize,encoding="unic")
-
-    while (font.getsize(text)[0] < img_fraction*width) and (stop == False):
-        # iterate until the text size is just larger than the criteria
-        if font.getsize(text)[0] == 0:
-            sel_font =  "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf"
- 
-            if font.getsize(text)[1] == 0:
-                stop = True
-                break
-
-        fontsize += 1
-        font = ImageFont.truetype(sel_font, fontsize,encoding="unic")
-
-    # optionally de-increment to be sure it is less than criteria
-    fontsize -= 1
-    font = ImageFont.truetype(sel_font, fontsize,encoding="unic")
-
-    return font
-
