a51def8b28025615ab00baecb6ab1a47178c5649
Bycarkos <cbonedriera@gmail.com>
2023-05-16 13:47:42 +0200

    Add file load models

--- SIDTD/data/DataLoader/Datasets.py
+++ SIDTD/data/DataLoader/Datasets.py
@@ -19,8 +19,7 @@ import cv2
 
 class Dataset(ABC):
     def __init__(self) -> None:
-        self._cluster_link = "http://datasets.cvc.uab.es"
-        
+        self._cluster_link = 'http://datasets.cvc.uab.es'
     @abstractmethod
     def num_fake_classes(self):
         raise NotImplementedError
@@ -73,20 +72,20 @@ class SIDTD(Dataset):
         
         ###### Static links
         self._uri = self._cluster_link + "/SIDTD"
-        self._images_path = "http://datasets.cvc.uab.es/SIDTD/templates.zip"
-        self._clips_path = "http://datasets.cvc.uab.es/SIDTD/clips.zip"
-        self._clips_cropped_path = "http://datasets.cvc.uab.es/SIDTD/clips_cropped.zip"
-        self._uri_videos = "http://datasets.cvc.uab.es/SIDTD/videos.zip"
-        self._uri_transfg_pretrained = "http://datasets.cvc.uab.es/SIDTD/imagenet21k+imagenet2012_ViT-L_16.zip"
+        self._images_path = os.path.join(self._uri,"templates.zip")
+        self._clips_path = os.path.join(self._uri,"clips.zip")
+        self._clips_cropped_path = os.path.join(self._uri,"clips_cropped.zip")
+        self._uri_videos = os.path.join(self._uri,"videos.zip")
 
         ## static Csv
-        self._uri_static_kfold_templates = 'http://datasets.cvc.uab.es/SIDTD/split_kfold.zip'
-        self._uri_static_kfold_clips = "http://datasets.cvc.uab.es/SIDTD/split_kfold_unbalanced.zip"
-        self._uri_static_kfold_cropped_clips = "http://datasets.cvc.uab.es/SIDTD/split_kfold_cropped_unbalanced.zip"
-        self._uri_static_normal_templates = 'http://datasets.cvc.uab.es/SIDTD/split_normal.zip'
-        self._uri_static_normal_clips = 'http://datasets.cvc.uab.es/SIDTD/cross_val_unbalanced.zip'
-        self._uri_static_shot_templates = 'http://datasets.cvc.uab.es/SIDTD/split_shot.zip'
-        self._uri_static_shot_clips = 'http://datasets.cvc.uab.es/SIDTD/split_shot_unbalanced.zip'
+
+        self._uri_static_kfold_templates = os.path.join(self._uri, 'split_kfold.zip')
+        self._uri_static_kfold_clips = os.path.join(self._uri,"split_kfold_unbalanced.zip")
+        self._uri_static_kfold_cropped_clips = os.path.join(self._uri,"split_kfold_cropped_unbalanced.zip")
+        self._uri_static_normal_templates = os.path.join(self._uri,'split_normal.zip')
+        self._uri_static_normal_clips = os.path.join(self._uri,'cross_val_unbalanced.zip')
+        self._uri_static_shot_templates = os.path.join(self._uri, 'split_shot.zip')
+        self._uri_static_shot_clips = os.path.join(self._uri, 'split_shot_unbalanced.zip')
         
 
 
@@ -96,12 +95,9 @@ class SIDTD(Dataset):
         
         #path to download
         self._path_to_download = os.path.join(os.getcwd(), "datasets")
-
         self._abs_path = os.path.join(self._path_to_download,os.path.basename(self._uri)) # cwd/datasets/SIDTD/...
         self.abs_path_code_ex_csv = os.path.join(os.getcwd(), "explore", "static")
-        self.abs_path_code_ex = os.path.join(os.getcwd(), "explore", "pretrained_models")
-        self.abs_path_trans_fg = os.path.join(os.getcwd(), "models", "transfg", "transfg_pretrained")
-            
+
   
     def _define_paths(self) ->None:        
         ## Path to reconstruct the original structure
@@ -194,85 +190,6 @@ class SIDTD(Dataset):
         else:
             print("OPTION: do not download dataset files") 
 
-    def download_models(self, unbalanced:bool=False, cropped:bool=False, type_models:str="transfg_img_net"):
-        
-        if not unbalanced:
-            base_folder = "balanced_templates_SIDTD"
-            arc_name = "/coatten_fcn_model_trained_models.zip"
-        else:
-            arc_name = "/coattention_trained_models.zip"
-            if cropped:
-                base_folder = "unbalanced_clip_cropped_SIDTD"
-            else:
-                base_folder = "unbalanced_clip_background_SIDTD"
-
-        
-        abs_path_model = os.path.join(self.abs_path_code_ex, base_folder)
-        server_path_model = os.path.join(self._uri, base_folder)
-
-        if type_models == "all_trained_models":   
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex,server_path_model +"/efficientnet-b3_trained_models.zip" ))
-            with zipfile.ZipFile(abs_path_model+"/efficientnet-b3_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex,server_path_model+"/resnet50_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/resnet50_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex, server_path_model+"/vit_large_patch16_224_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/vit_large_patch16_224_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex, server_path_model+"/trans_fg_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/trans_fg_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex, server_path_model+ arc_name))
-            with zipfile.ZipFile(abs_path_model+ arc_name, 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_trans_fg,self._uri_transfg_pretrained))
-            with zipfile.ZipFile(self.abs_path_trans_fg+"/imagenet21k+imagenet2012_ViT-L_16.zip", 'r') as zip_ref:
-                zip_ref.extractall(self.abs_path_trans_fg)
-
-        if type_models == "effnet":
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex,server_path_model+"/efficientnet-b3_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/efficientnet-b3_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-        elif type_models == "resnet":
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex,server_path_model+"/resnet50_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/resnet50_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-        elif type_models == "vit":
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex,server_path_model+"/vit_large_patch16_224_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/vit_large_patch16_224_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-        elif type_models == "transfg":
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex, server_path_model+ "/trans_fg_trained_models.zip"))
-            with zipfile.ZipFile(abs_path_model+"/trans_fg_trained_models.zip", 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_trans_fg,self._uri_transfg_pretrained))
-            with zipfile.ZipFile(self.abs_path_trans_fg+"/imagenet21k+imagenet2012_ViT-L_16.zip", 'r') as zip_ref:
-                zip_ref.extractall(self.abs_path_trans_fg)
-
-        elif type_models == "arc":
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_code_ex, server_path_model +  arc_name))
-            with zipfile.ZipFile(abs_path_model+ arc_name, 'r') as zip_ref:
-                zip_ref.extractall(abs_path_model)
-
-        elif type_models == "transfg_img_net":
-            os.system("bash -c 'wget -erobots=off -m -k --cut-dirs=1 -nH -P {} {}'".format(self.abs_path_trans_fg,self._uri_transfg_pretrained))
-            with zipfile.ZipFile(self.abs_path_trans_fg+"/imagenet21k+imagenet2012_ViT-L_16.zip", 'r') as zip_ref:
-                zip_ref.extractall(self.abs_path_trans_fg)
-
-        else:
-            print("OPTION: do not download trained model files") 
-           
-                          
     def create_structure_videos(self):
         videos_abs_path = os.path.join(self._abs_path, "videos", "reals")
         
