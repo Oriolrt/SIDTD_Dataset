98716c848f49fc137032ccbd34b3e9ea02250bd2
mtalarmain <mtalarmain@cvc.uab.cat>
2024-07-18 12:56:00 +0200

    fit fsl code to sidtd github code

--- SIDTD/models/fsl_models/utils/data_tools/__init__.py
+++ SIDTD/models/fsl_models/utils/data_tools/__init__.py
@@ -1,11 +1,5 @@
-from .easy_set_coaarc_random import EasySetCoAARCRandom
-from .easy_set_coaarc_random_path import EasySetCoAARCRandomPath
-from .easy_set_random import EasySetRandom
-from .easy_set_random_path import EasySetRandomPath
-
-
+from .easy_set_coaarc import EasySetCoAARCRandom
+from .easy_set import EasySetRandom
 from .task_sampler import TaskSampler
-from .task_sampler_path import TaskSamplerPath
 from .task_sampler_coaarc import TaskSamplerCoAARC
-from .task_sampler_coaarc_path import TaskSamplerCoAARCPath
 
